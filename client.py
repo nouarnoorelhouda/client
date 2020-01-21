37f127c73ec4197d59dac1833b73bd126ec49c53
nouarnoorelhouda <nouarnoorelhouda@gmail.com>
2020-01-21 08:31:28 +0100

    Update client.py

--- client.py
+++ client.py
@@ -4,12 +4,15 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 1111))
+while 1:
+    print('server sent:', s.recv(1024).decode())
+    s.send(b'Oi you sent something to me')
 
-print("Le nom du fichier que vous voulez récupérer:")
-file_name = input(">> ") # utilisez raw_input() pour les anciennes versions python
-s.send(file_name.encode())
-file_name = r"C:\Users\adminnennouar\PycharmProjects\VNFsTest\data\file%s" % (file_name,)
-r = s.recv(9999999)
-with open(file_name,'wb') as _file:
-    _file.write(r)
-print("Le fichier a été correctement copié dans : %s." % file_name)
+#print("Le nom du fichier que vous voulez récupérer:")
+#file_name = input(">> ") # utilisez raw_input() pour les anciennes versions python
+#s.send(file_name.encode())
+#file_name = r"C:\Users\adminnennouar\PycharmProjects\VNFsTest\data\file%s" % (file_name,)
+#r = s.recv(9999999)
+#with open(file_name,'wb') as _file:
+#    _file.write(r)
+print(r)
